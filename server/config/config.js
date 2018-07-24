0da9ab58ecfc8628a192b81029392293258cba66
lparra98 <l.parra03@ufromail.cl>
2018-07-24 16:05:48 -0400

    Cambios Rest Server

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,13 @@
 // Puerto
 // =====================
 process.env.PORT = process.env.PORT || 3000;
+
+// =====================
+// Entorno
+// =====================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =====================
+// Base de datos
+// =====================
+process.env.URLDB = (process.env.NODE_ENV == 'dev') ? 'mongodb://localhost:27017/cafe' : 'mongodb://cafeUser:bastian_12@ds147011.mlab.com:47011/cafe';
