e94db64596285afc482af5d88094784e68c9e343
lparra98 <l.parra03@ufromail.cl>
2018-07-24 16:43:38 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -11,4 +11,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // =====================
 // Base de datos
 // =====================
-process.env.URLDB = (process.env.NODE_ENV == 'dev') ? 'mongodb://localhost:27017/cafe' : 'mongodb://cafeUser:bastian_12@ds147011.mlab.com:47011/cafe';
+process.env.URLDB = (process.env.NODE_ENV == 'dev') ? 'mongodb://localhost:27017/cafe' : process.env.MONGO_URI;
